68ac7b0407cc37f58ccacd5177c04e007f8fb50b
uberfactsriddles2k20 <uberfacts_riddles2k20@protonmail.ch>
2020-10-07 15:30:28 -0700

    intro alert

---
+++ index_script.js
@@ -0,0 +1,3 @@
+function introAlert() {
+  alert("Hello! and welcome uberfans /uberfactors to our webcite!!! Dont forget to check /instagram (uber_factsriddles2k20. (PS press play on music!!!!)))");
+}
