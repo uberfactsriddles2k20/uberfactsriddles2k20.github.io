38845db5360536c1ebe2967181b888764a4a4a70
uberfactsriddles2k20 <uberfacts_riddles2k20@protonmail.ch>
2020-10-19 11:58:14 -0700

    pop ups

--- index_script.js
+++ index_script.js
@@ -1,3 +1,11 @@
 function introAlert() {
   alert("Hello! and welcome uberfans /uberfactors to our webcite!!! Dont forget to check /instagram (uber_factsriddles2k20. (PS press play on music!!!!)))");
 }
+
+function popUp() {
+	window.open("#loginScreen", "_self");
+}
+
+function activatePopUp() {
+	setInterval(popUp, 5000);
+}
