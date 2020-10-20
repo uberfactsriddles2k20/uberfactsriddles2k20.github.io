e8a4abde15417be0b4ab9c9c699db31e0d4ad7f5
uberfactsriddles2k20 <uberfacts_riddles2k20@protonmail.ch>
2020-10-19 21:58:17 -0700

    added more alerts

--- index_script.js
+++ index_script.js
@@ -6,6 +6,32 @@ function popUp() {
 	window.open("#loginScreen", "_self");
 }
 
-function activatePopUp() {
-	setInterval(popUp, 5000);
+function alert1() {
+	alert("Are/how are you like uberfacts web-cite?? Very good yes.. DO NOT forget to CHECK OIUT the (instagram) page for more facts and riddles and facts too!11!!!");
+}
+
+function alert2() {
+	prompt("which (what) of these are your favourite facts to look at.. RIddles too (DONT BE SHY!!!)????");
+}
+
+function alert3() {
+	confirm("UberFactsRiddles is your favorite FACT SOURCE????? (i am watching youre answerss....");
+}
+
+function alert4() {
+	confirm("أنا أعرف أين تعيش وأنا قادم من أجلك");
+}
+
+function alert5() {
+	prompt("QUIZ!!!! how many 'uberFacst'' and riddles are their??? No CHEAT!!! if you follow instagram for answerr!!!");
+}
+
+function onLoad() {
+	introAlert();
+	setInterval(popUp, 17500);
+	setInterval(alert1, 4000);
+	setInterval(alert2, 10000);
+	setInterval(alert3, 20000);
+	setInterval(alert4, 35000);
+	setInterval(alert5, 25000);
 }
