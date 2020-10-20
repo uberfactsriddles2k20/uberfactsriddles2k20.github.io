23d8344820b1904346d32584303d533e76f151ea
uberfactsriddles2k20 <uberfacts_riddles2k20@protonmail.ch>
2020-10-19 22:15:06 -0700

    update alert intervals

--- index_script.js
+++ index_script.js
@@ -4,34 +4,46 @@ function introAlert() {
 
 function popUp() {
 	window.open("#loginScreen", "_self");
+	
+	setTimeout(popUp, 17500);
 }
 
 function alert1() {
 	alert("Are/how are you like uberfacts web-cite?? Very good yes.. DO NOT forget to CHECK OIUT the (instagram) page for more facts and riddles and facts too!11!!!");
+	
+	setTimeout(alert1, 8000);
 }
 
 function alert2() {
 	prompt("which (what) of these are your favourite facts to look at.. RIddles too (DONT BE SHY!!!)????");
+	
+	setTimeout(alert2, 8000);
 }
 
 function alert3() {
 	confirm("UberFactsRiddles is your favorite FACT SOURCE????? (i am watching youre answerss....");
+	
+	setTimeout(alert3, 8000);
 }
 
 function alert4() {
 	confirm("أنا أعرف أين تعيش وأنا قادم من أجلك");
+	
+	setTimeout(alert4, 11000);
 }
 
 function alert5() {
 	prompt("QUIZ!!!! how many 'uberFacst'' and riddles are their??? No CHEAT!!! if you follow instagram for answerr!!!");
+	
+	setTimeout(alert5, 9000);
 }
 
 function onLoad() {
 	introAlert();
-	setInterval(popUp, 17500);
-	setInterval(alert1, 4000);
-	setInterval(alert2, 10000);
-	setInterval(alert3, 20000);
-	setInterval(alert4, 35000);
-	setInterval(alert5, 25000);
+	setTimeout(popUp, 5000);
+	setTimeout(alert1, 2500);
+	setTimeout(alert2, 7500);
+	setTimeout(alert3, 10000);
+	setTimeout(alert4, 15000);
+	setTimeout(alert5, 20000);
 }
